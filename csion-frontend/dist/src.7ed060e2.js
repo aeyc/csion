4095ac44817ceb2d17091c51eb64b861eb185c99
Nursena Kurubas <nursena.kurubas@ug.bilkent.edu.tr>
2020-02-15 16:56:23 +0300

    frontend

--- csion-frontend/dist/src.7ed060e2.js
+++ csion-frontend/dist/src.7ed060e2.js
@@ -124,7 +124,7 @@ $("button").click(function () {
     $("button").html(data);
   });
 });
-},{}],"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../../../Users/z00435eb/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -152,7 +152,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51546" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53139" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -328,5 +328,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","../src/index.js"], null)
+},{}]},{},["../../../../../../../Users/z00435eb/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","../src/index.js"], null)
 //# sourceMappingURL=/src.7ed060e2.js.map
