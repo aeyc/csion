35707d95b07147744a69351ced1e21ce528c9a8d
mehmetselimozcan <43796886+mehmetselimozcan@users.noreply.github.com>
2020-03-02 21:16:57 +0300

    Add Questions schema

--- csion-backend/Managers/DatabaseManager.js
+++ csion-backend/Managers/DatabaseManager.js
@@ -1,11 +1,42 @@
 const Personality = require("../model/Personality");
+const NormalQuestion = require("../model/NormalQuestions");
+
+
+//getPersonalityType('Di');
 
 
 
-    Personality.find({type: 'ISTP'}, (err, kullanicilar) => {
+function getPersonalityType(type) {
+    Personality.find({type: type}, (err, personality) => {
         if (err) throw err;
-        console.log(kullanicilar);
+        console.log(personality);
     });
+}
+
+
+
+
+createNormalQuestion(null,null,null,null,null,null, null, null, false);
+
+function createNormalQuestion(question, requiredTags,providedPositiveTags,providedNegativeTags, keywords, score, subject, subSubject, isInitial){
+    let Normal =  new NormalQuestion({
+        question: question,
+        requiredTags: requiredTags,
+        providedPositiveTags: providedPositiveTags,
+        providedNegativeTags: providedNegativeTags,
+        keywords: keywords,
+        score: score,
+        subject: subject,
+        subSubject: subSubject,
+        isInitial: isInitial,
+    });
+
+    Normal.save((err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+
 
 
 
