7f23859849317231940e6317e21d445db58c1bb7
mehmetselimozcan <43796886+mehmetselimozcan@users.noreply.github.com>
2020-03-04 17:13:00 +0300

    Update personality function

--- csion-backend/Managers/DatabaseManager.js
+++ csion-backend/Managers/DatabaseManager.js
@@ -16,7 +16,7 @@ function getPersonalityType(type) {
 
 
 
-createNormalQuestion(null,null,null,null,null,null, null, null, false);
+//createNormalQuestion(null,null,null,null,null,null, null, null, false);
 
 function createNormalQuestion(question, requiredTags,providedPositiveTags,providedNegativeTags, keywords, score, subject, subSubject, isInitial){
     let Normal =  new NormalQuestion({
@@ -37,6 +37,39 @@ function createNormalQuestion(question, requiredTags,providedPositiveTags,provid
     });
 }
 
+
+function updatePersonality(type,tends, strengths,weaknesses,growths,motivations,stresses,positiveCareer,negativeCareer,jobs,positiveFriendship,negativeFriendship,positiveRelationship,negativeRelationship,keywords) {
+
+    Personality.update({ type: type  }, { tends: tends, strengths: strengths,
+    weaknesses: weaknesses, growths: growths, motivations: motivations,
+    Stresses: stresses, positiveCareer: positiveCareer, negativeCareer: negativeCareer,
+    jobs: jobs, positiveFriendship: positiveFriendship,  negativeFriendship: negativeFriendship,
+    positiveRelationship: positiveRelationship, negativeRelationship: negativeRelationship, keywords: keywords})
+        .exec(function(err, personality) {
+            console.log(personality);
+        });
+
+}
+
+let type = "D";
+let tends = ['Pursuing goals', 'Resourceful', 'Competition', 'High Expectations','Debate','Resist Influence'];
+let strengths = ['Communicate Directly', 'Realistic Expectations', 'Conclusive','Goal Oriented','Urgency', 'Motivating Others','Directing Others','Control'];
+let weaknesses = ['Teamwork' , 'Omitting Details', 'Displaying Impatience', 'Criticize Others', 'Aggressive'];
+let growths = [];
+let motivations = [];
+let stresses = [];
+let positiveCareer = [];
+let negativeCareer = [];
+let jobs = [];
+let positiveFriendship = [];
+let negativeFriendship = [];
+let positiveRelationship = [];
+let negativeRelationship = [];
+let keywords = [];
+
+
+updatePersonality(type,tends,strengths,weaknesses,growths,motivations,stresses,positiveCareer,negativeCareer,jobs,positiveFriendship,
+    negativeFriendship,positiveRelationship,negativeRelationship,keywords);
 
 
 
