c82c0ba4270c2ea8bf256a44cc8947bd78f2d6b8
mehmetsan <trabzonpower@gmail.com>
2020-03-15 18:30:20 +0300

    adding personality type in python

---
+++ addPersonalityType.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 17:14:38 2020
+
+@author: MehmetSanisoglu
+"""
+
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://mehmetsan:Northern61@clustermehmet-aio9p.mongodb.net/test?retryWrites=true&w=majority")
+db = cluster["Personality"]
+collection = db["test"]
+
+typeId = ""
+tends = []
+strengths = []
+weaknesses = []
+growths = []
+motivations = []
+Stresses = []
+positiveCareer = []
+negativeCareer = []
+jobs = []
+positiveFriendship = []
+negativeFriendship = []
+positiveRelationship = []
+negativeRelationship = []
+keywords = []
+typeCategory = typeId
+typeName = ""
+
+def enterAttribute(attributeName, name):
+    check = True
+
+    while(check != "0"):
+        prompt = "Enter "+name+" (Press 0 for exit): "
+        check = input(prompt)
+        if(check != "0"):
+            attributeName.append(check)
+    return
+
+menuCheck = True
+while(menuCheck):
+    print("New Personality")
+    typeId = input("Enter tpyeID, same as type:")
+    enterAttribute(tends,"tends")
+    enterAttribute(strengths, "strengths")
+    enterAttribute(weaknesses,"weaknesses")
+    enterAttribute(growths,"growths")
+    enterAttribute(motivations,"motivations")
+    enterAttribute(Stresses,"Stresses")
+    enterAttribute(positiveCareer,"positiveCareer")
+    enterAttribute(negativeCareer,"negativeCareer")
+    enterAttribute(jobs,"jobs")
+    enterAttribute(positiveFriendship,"positiveFriendship")
+    enterAttribute(negativeFriendship,"negativeFriendship")
+    enterAttribute(positiveRelationship,"positiveRelationship")
+    enterAttribute(negativeRelationship,"negativeRelationship")
+    enterAttribute(keywords,"keywords")
+    typeName = input("Enter type name, e.g. The Editor:")
+    menuCheck = int(input("This personality is over. Continue with new personality? 0=NO, 1=YES :"))
+
+post = {"_id": typeId, "tends": tends, "strengths": strengths, "weaknesses": weaknesses, "growths": growths, "motivations": motivations, "Stresses": Stresses,
+         "positiveCareer": positiveCareer, "negativeCareer": negativeCareer, "jobs": jobs, "positiveFriendship": positiveFriendship, "negativeFriendship": negativeFriendship,
+         "negativeRelationship": negativeRelationship, "keywords": keywords, "type": typeCategory, "name": typeName, "__v":0
+         }
+
+collection.insert_one(post)
