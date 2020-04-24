a596a7cb8e9da35db053521c7f60937db0967e32
aeyc <abegumtascioglu@gmail.com>
2020-04-24 12:24:27 +0300

    types are fixed for database

--- Data/Personality Data/personality.py
+++ Data/Personality Data/personality.py
@@ -12,11 +12,15 @@ import json
 
 df = pd.read_csv("C_Types.csv",sep=';',dtype=str)
 
+for i in range(0,len(df)):
+    for j in df.columns:
+        if j != "type" and j != "name":
+            (df.iloc[i])[j] = (df.iloc[i])[j].split(",")
 try: 
     cluster = MongoClient("mongodb+srv://mehmetsan:Northern61@clustermehmet-aio9p.mongodb.net/test?retryWrites=true&w=majority")
     print("Connected") 
     db = cluster["Personality"]
-    collection = db["test"]
+    collection = db["personalities"]
     records = json.loads(df.T.to_json()).values()
     collection.insert(records)
 except:   
